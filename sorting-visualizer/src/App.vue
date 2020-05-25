156b63019b31a81999bbac89ec54ab23050fc1d7
taufiqhusada <taufiqhusdr@gmail.com>
2020-05-25 17:17:45 +0700

    udah bisa basic

---
+++ sorting-visualizer/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Bubble Sort Visualizer App"/>
+    <InputForm />
+    <Result />
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+  import InputForm from './components/InputForm.vue'
+  import Result from './components/Result.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      InputForm,
+      Result
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ sorting-visualizer/src/components/InputForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="inputForm">
+    <form v-on:submit="submitForm" id="inputArrayForm">
+      <label>Number of Elements:</label>
+      <input type="text" class="input" name="arraySize"/>
+      <br>
+      <label>Array Elements:</label>
+      <input type="text" class="input" name="arrayElements"/>
+      <br>
+      <button type="submit">Submit</button>
+    </form>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputForm',
+  methods: {
+    submitForm(event) {
+      event.preventDefault();
+      const inputArrayForm = document.getElementById('inputArrayForm');
+      const arraySize = inputArrayForm.querySelector('input[name = arraySize]');
+      const arrayElements = inputArrayForm.querySelector('input[name = arrayElements]');
+      this.$root.$emit('inputData',arraySize.value, arrayElements.value);
+    }
+  }
+}
+</script>
+
