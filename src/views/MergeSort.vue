f87230f92bff714d3bc606c9f400a9fdd272dca5
michaelhans <michaelhans777@gmail.com>
2020-06-12 16:02:14 +0700

    Update MergeSort.vue

--- src/views/MergeSort.vue
+++ src/views/MergeSort.vue
@@ -6,7 +6,7 @@
       Konsep <i>Divide and Conquer</i> yang dimaksud adalah dengan membagi data S dalam dua kelompok, yaitu S1 dan S2 yang tidak beririsan (<i>disjoint</i>).
       Proses pembagian data dilakukan secara rekursif sampai data tidak dapat dibagi lagi atau dengan kata lain data dalam sub bagian menjadi tunggal.
       <br><br>
-      Setelah data tidak dapat dibagi lagi, proses penggabungan (merging) dilakukan antara sub-sub bagian dengan memperhatikan urutan data yang diinginkan (<i>ascending</i> / kecil ke besar
+      Setelah data tidak dapat dibagi lagi, proses penggabungan (<i>merging</i>) dilakukan antara sub-sub bagian dengan memperhatikan urutan data yang diinginkan (<i>ascending</i> / kecil ke besar
        atau <i>descending</i> / besar ke kecil).
       Proses penggabungan ini dilakukan sampai semua data tergabung dan terurut sesuai urutan yang diiginkan. Kompleksitas dari algoritma Merge Sort adalah O(n log n).
       </p>
